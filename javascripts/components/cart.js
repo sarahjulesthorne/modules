2999d3d2380b48785c2511075a1aef5125dddb3c
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-05-03 20:29:07 -0500

    WIP partially finished notes/readme

--- javascripts/components/cart.js
+++ javascripts/components/cart.js
@@ -1,13 +1,23 @@
-
+/*module created to hold cart functionality
+Used getter function for security, setter function to build array,
+buy event function to display total price in alert on button click,
+And cartToDom function to display contents of cart and offer purchase button with click functionality*/
 import util from '../helpers/util.js';
+
+//created to hold info about product in cart
 const cart = [];
+
+//created to build cart array with product info on button click
 const setCart = (book) => {
 cart.push(book);
 };
+
+//created to pass cart info to other functions/modules securely
 const getCart = () => {
 return cart;
 };
 
+//on click event, used reduce to return total price of all products in cart in order to be able to display alert with total amount in message
 const buyEvent = (event) => {
 event.preventDefault();
 const myCart = getCart();
@@ -18,11 +28,10 @@ return a + b.price;
 window.alert(`You owe ${total.toFixed(2)}`);
 };
 
+//used domString and print functions to create initial cart content, in order to display contents to user.
+//Also added click event to purchase button so user can click and see total price of items in cart
 const cartToDom = () => {
-
-    
 const myCart = getCart();
-console.log(myCart);
   let domString = '<div class="row">';
   domString += '<h2>Cart:</h2>';
   domString += '<button id="purchase-btn" class="btn btn-secondary">Purchase</button>'
